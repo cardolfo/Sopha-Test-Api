85ed22044395c7d4ae5a6cfbbf12fa89bdc706b7
Carlos Adolfo Rebouças Gondim <cadolfo2sparck@gmail.com>
2022-11-06 08:01:12 -0300

    Deploy with Puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,16 @@ namespace :puma do
 end
 
 namespace :deploy do
+  desc 'Make sure local git is in sync with remote.'
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts 'WARNING: HEAD is not the same as origin/master'
+        puts 'Run `git push` to sync changes.'
+        exit
+      end
+    end
+  end
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
@@ -42,6 +52,7 @@ namespace :deploy do
       invoke!('puma:restart')
     end
   end
+  before :starting, :check_revision
   after :finishing, :cleanup
   after :finishing, :restart
 end
